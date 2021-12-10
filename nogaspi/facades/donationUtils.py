ba5f40723e72599741c302e455ce914b340b730e
Clement Sergent <clementsergent@yahoo.fr>
2021-12-10 12:28:12 +0100

    kolimeter to meters

---
+++ nogaspi/facades/donationUtils.py
@@ -0,0 +1,15 @@
+import geopy
+import geopy.distance
+from geopy.units import meters
+
+def getCoordMinMaxAroundDistance(coordStart, distance):
+
+    start = geopy.Point(coordStart)
+    d = geopy.distance.distance(meters = distance)
+
+    latMin = d.destination(point=start, bearing=180).latitude
+    latMax = d.destination(point=start, bearing=0).latitude
+    lonMin = d.destination(point=start, bearing=270).longitude
+    lonMax = d.destination(point=start, bearing=90).longitude
+
+    return latMin, latMax, lonMin, lonMax
