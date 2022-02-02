179b4a697eec31bf74ba02c981169683efb42bfd
Clement Sergent <clementsergent@yahoo.fr>
2022-02-02 22:31:12 +0100

    test

--- test/functionalTest/test_register_checkTokenValidity.py
+++ test/functionalTest/test_register_checkTokenValidity.py
@@ -1,18 +1,29 @@
 from nogaspi.views.register.checkTokenValidity import checkTokenValidityTest
-from nogaspi.dbEngine import EngineSQLAlchemy
+from test.functionalTest.dbMagement import sqlQuery, DBTestLifeCicle
+import datetime
 
 def test_register_checkTokenValidity():
+    with DBTestLifeCicle():
+        timeAfterNow = datetime.datetime.now() + datetime.timedelta(minutes = 15)
+        sqlQuery(f"INSERT INTO `user` (`id`, `mail`, `password`, `pseudo`, `profilePicture`, `token`, `token_expiration`, `idRang`, `points`, `regularPathLatitudeStart`, `regularPathLongitudeStart`, `regularPathLatitudeEnd`, `regularPathLongitudeEnd`, `regularPathPoints`, `fireBaseToken`) VALUES (NULL, 'toto@toto.fr', 'toto', 'toto', NULL, 'token_toto', '{timeAfterNow}', NULL, '0', NULL, NULL, NULL, NULL, NULL, NULL);")
+        fnrtr = checkTokenValidityTest("token_toto")
 
-    with EngineSQLAlchemy() as session:
-        session.execute("DROP DATABASE IF EXISTS `nogaspi`;")
-        with open("test/nogaspi.sql", encoding="utf-8") as sql_file:
-            for statement in sql_file.read().split(';'):
-                if len(statement.strip()) > 0:
-                    session.execute(statement + ';')
-        session.commit()
+    assert fnrtr['validity']
+    assert fnrtr['user'] == 'toto@toto.fr'
 
-    with EngineSQLAlchemy() as session:
-        session.execute("INSERT INTO `user` (`id`, `mail`, `password`, `pseudo`, `profilePicture`, `token`, `token_expiration`, `idRang`, `points`, `regularPathLatitudeStart`, `regularPathLongitudeStart`, `regularPathLatitudeEnd`, `regularPathLongitudeEnd`, `regularPathPoints`, `fireBaseToken`) VALUES (NULL, 'toto@toto.fr', 'toto', 'toto', NULL, 'token_toto', '2022-02-02 19:26:32.000000', NULL, '0', NULL, NULL, NULL, NULL, NULL, NULL);")
-        session.commit()
-    test = checkTokenValidityTest("token_toto")
-    assert test['validity'] == False
+def test_register_checkTokenValidity_with_bad_Token():
+    with DBTestLifeCicle():
+        timeAfterNow = datetime.datetime.now() + datetime.timedelta(minutes = 15)
+        sqlQuery(f"INSERT INTO `user` (`id`, `mail`, `password`, `pseudo`, `profilePicture`, `token`, `token_expiration`, `idRang`, `points`, `regularPathLatitudeStart`, `regularPathLongitudeStart`, `regularPathLatitudeEnd`, `regularPathLongitudeEnd`, `regularPathPoints`, `fireBaseToken`) VALUES (NULL, 'toto@toto.fr', 'toto', 'toto', NULL, 'token_toto', '{timeAfterNow}', NULL, '0', NULL, NULL, NULL, NULL, NULL, NULL);")
+        fnrtr = checkTokenValidityTest("token_tata")
+
+    assert fnrtr == {'validity': False, 'user' : 'Unknown', 'token_expiration': 'Unknown'}
+    
+def test_register_checkTokenValidity_with_bad_expirationDate():
+    with DBTestLifeCicle():
+        timeAfterNow = datetime.datetime.now() - datetime.timedelta(minutes = 15)
+        sqlQuery(f"INSERT INTO `user` (`id`, `mail`, `password`, `pseudo`, `profilePicture`, `token`, `token_expiration`, `idRang`, `points`, `regularPathLatitudeStart`, `regularPathLongitudeStart`, `regularPathLatitudeEnd`, `regularPathLongitudeEnd`, `regularPathPoints`, `fireBaseToken`) VALUES (NULL, 'toto@toto.fr', 'toto', 'toto', NULL, 'token_toto', '{timeAfterNow}', NULL, '0', NULL, NULL, NULL, NULL, NULL, NULL);")
+        fnrtr = checkTokenValidityTest("token_toto")
+
+    assert not fnrtr['validity']
+    assert fnrtr['user'] == 'toto@toto.fr'
