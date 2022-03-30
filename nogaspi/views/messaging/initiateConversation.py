33b9cf5c7974040a2c8257664ab77edd82c95908
clement <clementsergent@yahoo.fr>
2022-03-30 16:22:49 +0200

    new id on route initiateConversation

--- nogaspi/views/messaging/initiateConversation.py
+++ nogaspi/views/messaging/initiateConversation.py
@@ -40,6 +40,6 @@ def initiateConversation(request):
 
         fbMessage_newConversation(user, userOwner, conversation, firstMessage)
 
-        data = {'isInitiate':True}
+        data = {'isInitiate':True, 'idNewConversation': conversation.id}
 
     return data
